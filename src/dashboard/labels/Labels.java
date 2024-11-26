a7a7a93d6d68f26cec145757b4f1646864054593
eyronc <aaroncumahig12@gmail.com>
2024-11-26 19:40:48 +0800

    Description

--- src/dashboard/labels/Labels.java
+++ src/dashboard/labels/Labels.java
@@ -10,35 +10,32 @@ public class Labels {
         mainPanel.setBackground(Color.decode("#232529"));
         mainPanel.setLayout(null);
         JLabel availableLabel, inSessionLabel, outOfTimeLabel, maintenanceLabel;
-    
-        availableLabel = new JLabel();
-        availableLabel.setText("<html><div style='text-align: center;'><span style='color: #00FF00; font-size: 14px;'>•</span> AVAILABLE</div></html>");
-        availableLabel.setForeground(Color.WHITE);
-        availableLabel.setFont(new Font("Inter", Font.BOLD, 10)); 
-        availableLabel.setBounds(0, 80, 180, 30);
+        
+        String text = "<html><div style='text-align: center;'><span style='color: #00FF00; font-size: 14px;'>•</span> AVAILABLE</div></html>";
+        availableLabel = createLabel(0, 80, 180, 30, text);
         mainPanel.add(availableLabel);
 
-        inSessionLabel = new JLabel();
-        inSessionLabel.setText("<html><div style='text-align: center;'><span style='color: yellow; font-size: 14px;'>•</span> IN-SESSION</div></html>");
-        inSessionLabel.setForeground(Color.WHITE);
-        inSessionLabel.setFont(new Font("Inter", Font.BOLD, 10));
-        inSessionLabel.setBounds(0, 95, 180, 30);
+        text = "<html><div style='text-align: center;'><span style='color: yellow; font-size: 14px;'>•</span> IN-SESSION</div></html>";
+        inSessionLabel = createLabel(0, 95, 180, 30, text);
         mainPanel.add(inSessionLabel);
 
-        outOfTimeLabel = new JLabel();
-        outOfTimeLabel.setText("<html><div style='text-align: center;'><span style='color: red; font-size: 14px;'>•</span> OUT OF TIME</div></html>");
-        outOfTimeLabel.setForeground(Color.WHITE);
-        outOfTimeLabel.setFont(new Font("Inter", Font.BOLD, 10));
-        outOfTimeLabel.setBounds(0, 110, 180, 30);
+        text = "<html><div style='text-align: center;'><span style='color: red; font-size: 14px;'>•</span> OUT OF TIME</div></html>";
+        outOfTimeLabel = createLabel(0, 110, 180, 30, text);
         mainPanel.add(outOfTimeLabel);
 
-        maintenanceLabel = new JLabel();
-        maintenanceLabel.setText("<html><div style='text-align: center;'><span style='color: #FFC0CB; font-size: 14px;'>•</span> MAINTENANCE</div></html>");
-        maintenanceLabel.setForeground(Color.WHITE);
-        maintenanceLabel.setFont(new Font("Inter", Font.BOLD, 10));
-        maintenanceLabel.setBounds(0, 125, 180, 30);
+        text = "<html><div style='text-align: center;'><span style='color: #FFC0CB; font-size: 14px;'>•</span> MAINTENANCE</div></html>";
+        maintenanceLabel = createLabel(0, 125, 180, 30, text);
         mainPanel.add(maintenanceLabel);
 
         return mainPanel;
+    }
+
+    public static JLabel createLabel(int x, int y, int w, int h, String text){
+        JLabel label = new JLabel();
+        label.setText(text);
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("Inter", Font.BOLD, 10)); 
+        label.setBounds(x,y,w,h);
+        return label;
     }
 }
